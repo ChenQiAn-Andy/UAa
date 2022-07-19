8b4781b5e38267cd0623755b0766adf0a57c4b64
Andy Chen <qcenzo@gmail.com>
2022-07-19 17:02:42 +0800

    UVa455

---
+++ UVa455.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UVa_OJ
+{
+    internal class UVa455 : IAlgorithm
+    {
+        public void Exec( )
+        {
+            char[ ] seq = Console.ReadLine( ).ToArray( );
+            int time = 1, n = seq.Length;
+            while ( time < n )
+            {
+                for ( int i = 0 ; i < time ; i++ )
+                {
+                    char ch = seq[ i ];
+                    for ( int j = time ; j < n ; j += time )
+                    {
+                        if ( ch != seq[ j ] )
+                        {
+                            goto NEXT;
+                        }
+                    }
+                    goto END;
+                }
+            NEXT:
+                time++;
+            }
+        END:
+            Console.WriteLine( time );
+        }
+    }
+}
